3837d232ca0947072c09c629013e4397c44d7d43
Josh Brown <39962766+jbrown1234@users.noreply.github.com>
2020-11-26 11:36:46 -0500

    Create Chapter_12_Example_02_Images.py
    Adding a brief example that shows an alternate way to call up and use images in the UI.

---
+++ Chapter_12/Chapter_12_Example_02_Images.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Images with Python on the PIL")
+root.geometry("500x200")
+
+root.mainloop()
